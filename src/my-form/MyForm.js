f5c9c4382ecbf8353ba9390b22ac9fcf354071a2
CeAlCuadrado <cealcuadrado@outlook.com>
2021-10-25 19:02:11 -0300

    Implementado control de la acción submit en el formulario

--- src/my-form/MyForm.js
+++ src/my-form/MyForm.js
@@ -4,15 +4,32 @@ import './MyForm.css';
 function MyForm() {
     const [name, setName] = useState('');
 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert(`Nombre ingresado: ${name}`)
+    };
+
     return (
-        <div className="form">
-            <form>
-                <label>Name</label>
-                <input type="text" class="form-control" value={name} onChange={(e) => setName(e.target.value)}/>
-            </form>
-            <hr />
-            <p>Name: {name}</p>
-        </div>
+      <div className="form">
+        <form onSubmit={handleSubmit}>
+          <div className="form-group mb-2">
+            <label>Name</label>
+            <input
+              type="text"
+              class="form-control"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            </div>
+            <div className="form-group">
+                <button type="submit" className="btn btn-primary">
+                    Enviar
+                </button>
+            </div>
+        </form>
+        <hr />
+        <p>Name: {name}</p>
+      </div>
     );
 }
 
